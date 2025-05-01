34a6963fb14854acbd7cc2cf7b8778536d542362
sexywonman <CaitlinxFlecker2@email.com>
2025-05-01 14:57:34 -0500

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,33 @@
+$("#click-me").click(function () {
+	console.log(Math.floor(Math.random() * 4) + 
+	1)
+	let val = 1
+	val = Math.floor(Math.random() * 4) + 1
+	switch (val) {
+	case 1:
+	console.log("GET LAID NOW")
+	$("#click-me").html
+	location.href='https://aoemgd.unfamiiliardate.net/?utm_source=da57dc555e50572d&ban=other&j5=1&j6=1&s1=198903&s2=2117065&s3=opps&s5=Opps&click_id=lol-Opps';
+	break
+	case 2:
+	console.log("GET LAID NOW")
+	$("#click-me").html
+	location.href='https://wq7prrr.bestmatchesdates.com/c3kw7cc?m=1&s1=Come&cid=Xx';
+	break
+	case 3:
+	console.log("GET LAID NOW")
+	$("#click-me").html
+	location.href='https://paysale.g2afse.com/click?pid=23710&offer_id=7852';
+	break
+	case 4:
+	console.log("GET LAID NOW")
+	$("#click-me").html
+	location.href='https://paysale.g2afse.com/click?pid=23710&offer_id=10803';
+	break
+	default:
+	console.log("GET LAID NOW")
+	$("#click-me").html
+    location.href='https://aoemgd.unfamiiliardate.net/?utm_source=da57dc555e50572d&ban=other&j5=1&j6=1&s1=198903&s2=2117065&s3=opps&s5=Opps&click_id=lol-Opps';
+	break
+}
+})
